6566f6e318d8aa22de4c7ff183a8616ecf0298c3
webcreatorid <hafidzabudi27@gmail.com>
2026-07-30 23:44:10 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const openButton = document.getElementById("openInvitation");
 
+    if (openButton) {
+        openButton.addEventListener("click", function () {
+            alert("Undangan berhasil dibuka!");
+        });
+    }
+});
